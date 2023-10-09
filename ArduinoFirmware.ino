3cbb5261030498e4a1edff5a813f8756a67dfe8c
mdodge2 <mdodge@hawaii.edu>
2023-10-09 13:20:41 -1000

    preliminary versions of near-complete firmware

--- ArduinoFirmware.ino
+++ ArduinoFirmware.ino
@@ -2,56 +2,68 @@
 // Fall 2023 Senior Design Project
 // Michael Dodge II
 
-#include "./ArduinoJson.h"
+#include "./RunExperiment.h"
 
-#define greenButtonPin       2;  // Digital pin for the green button
-#define redButtonPin         4;  // Digital pin for the red button
-#define shutdownButtonPin    6;  // Digital pin for the shutdown button
-#define torqueSensorPin      A0; // Analog pin for the torque sensor
-#define temperatureSensorPin A2; // Analog pin for the temperature sensor
+#define greenButtonPin       2  // Digital pin for the green button
+#define redButtonPin         3  // Digital pin for the red button
+#define shutdownButtonPin    4  // Digital pin for the shutdown button
+#define torqueSensorPin      A0 // Analog pin for the torque sensor
+#define temperatureSensorPin A2 // Analog pin for the temperature sensor
 
-#define linearActuator
+#define rotationDirectionPin 5  // Digital PWM pin for the rotation motor's direction
+#define rotationPulsePin     6  // Digital PWM pin for the rotation motor's motion
+#define linearDirectionPin   9  // Digital PWM pin for the linear motor's direction
+#define linearPulsePin       10 // Digital PWM pin for the linear motor's motion
 
-uint32_t cycle = 0; // How many times the loop has completed
+#define rotationPulseRate 150 // Hertz
+#define linearPulseRate   150 // Hertz
 
-DynamicJsonDocument data(1024);
-/*
- * unsigned 32-bit int c: the amount of cycles since the start of the operational period
- * float t: the amount of time since the start of the operational period
- * int gb: the boolean state of the digital pin for the green button
- * int rb: the boolean state of the digital pin for the red button
- * int sdb: the boolean state of the digital pin for the shutdown button
- * int 
- */
+#define timeBetweenRotationDirectionToggles 1000 // Milliseconds
+#define timeBetweenLinearDirectionToggles   1000 // Milliseconds
+
+
 
 void setup() {
   // Start serial port at given baudrate
   Serial.begin(9600);
   // Set pin modes
-  pinMode(greenButtonPin, INPUT)
-  pinMode(redButtonPin, INPUT)
-  pinMode(shutdownButtonPin, INPUT)
-  pinMode(torqueSensorPin, INPUT)
-  pinMode(temperatureSensorPin, INPUT)
+  pinMode(greenButtonPin, INPUT);
+  pinMode(redButtonPin, INPUT);
+  pinMode(shutdownButtonPin, INPUT);
+  pinMode(torqueSensorPin, INPUT);
+  pinMode(temperatureSensorPin, INPUT);
 }
 
 void loop() {
-  // Get current cycle and time stamp
-  data["c"] = cycle;
-  data["t"] = millis();
-  
-  // Get state of buttons
-  data["gb"] = (bool)digitalRead(greenButtonPin);
-  data["rb"] = (bool)digitalRead(redButtonPin);
-  data["sdb"] = (bool)digitalRead(shutdownButtonPin);
-
-  // Get sensor values: raw 10-bit form (as the Pi is better-equipped for math)
-  data["tq"] = analogRead(torqueSensorPin);
-  data["tm"] = analogRead(temperatureSensorPin);
-
-  // Print to Serial Port
-  serializeJson(data, Serial);
-  Serial.print("\n");
-  
-  cycle += 1;
+  if (digitalRead(greenButtonPin)) {
+    uint8_t experimentExitCode = runExperiment(
+     greenButtonPin,
+     redButtonPin,
+     shutdownButtonPin,
+     torqueSensorPin,
+     temperatureSensorPin,
+     rotationPulseRate,
+     linearPulseRate,
+     timeBetweenRotationDirectionToggles,
+     timeBetweenLinearDirectionToggles,
+     rotationDirectionPin,
+     rotationPulsePin,
+     linearDirectionPin,
+     linearPulsePin
+    );
+    switch (experimentExitCode) {
+      case 0: // red button
+        break;
+      case 1: // shutdown button
+        while(true) {
+          Serial.println("Shutdown pending.");
+          delay(1000);
+        }
+      default: // unknown error
+        while(true){
+          Serial.println("Something bad happened.");
+          delay(1000);
+        }
+    }
+  }
 }
---
+++ RunExperiment.h
@@ -0,0 +1,110 @@
+#pragma once
+#include "./ArduinoJson.h"
+// Module to support the firmware for the Arduino Uno in the Lava Viscometer
+// Fall 2023 Senior Design Project
+// Michael Dodge II
+
+uint8_t runExperiment(
+  int greenButtonPin,
+  int redButtonPin,
+  int shutdownButtonPin,
+  int torqueSensorPin,
+  int temperatureSensorPin,
+  int rotationPulseRate,
+  int linearPulseRate,
+  int timeBetweenRotationDirectionToggles,
+  int timeBetweenLinearDirectionToggles,
+  uint8_t rotationDirectionPin,
+  uint8_t rotationPulsePin,
+  uint8_t linearDirectionPin,
+  uint8_t linearPulsePin) {
+  /*
+   * Returns 0 if terminated by red button
+   * Returns 1 if terminated by unknown/error
+   * Returns 2 if terminated by shutdown button
+  */
+  DynamicJsonDocument data(512);
+  /*
+   * unsigned 32-bit int c: the amount of cycles since the start of the operational period
+   * float t: the amount of time since the start of the operational period
+   * int gb: the boolean state of the digital pin for the green button
+   * int rb: the boolean state of the digital pin for the red button
+   * int sdb: the boolean state of the digital pin for the shutdown button
+   * int tq: the raw analog (0-1023) value of the torque sensor
+   * int tm: the raw analog (0-1023) value of the temperature sensor
+  */
+  uint32_t cycle = 0; // How many times the loop has completed
+  bool rotationDirection = true; // CW when >= 0, CCW otherwise
+  bool linearDirection = true;   // Forward when >= 0, Backward otherwise
+  uint32_t lastRotationDirectionToggleTime, lastLinearDirectionToggleTime;
+  data["rb"] = false;
+  data["sdb"] = false;
+
+  while (!data["rb"] || !data["sdb"] || cycle <= 10000) { // cycle count prevents infinite loop
+    // Get current cycle and time stamp
+    data["c"] = cycle;
+    data["t"] = (uint32_t)millis();
+  
+    // Get state of buttons
+    data["gb"] = (bool)digitalRead(greenButtonPin);
+    data["rb"] = (bool)digitalRead(redButtonPin);
+    data["sdb"] = (bool)digitalRead(shutdownButtonPin);
+
+    // Get sensor values: raw 10-bit form (as the Pi is better-equipped for math)
+    data["tq"] = (int)analogRead(torqueSensorPin);
+    data["tm"] = (int)analogRead(temperatureSensorPin);
+
+
+    /* Motor Direction Toggling */
+    bool rotationDirectionHasResetThisPeriod = false;
+    bool linearDirectionHasResetThisPeriod = false;
+
+    // Rotation
+    if (!rotationDirectionHasResetThisPeriod) {
+      rotationDirection = !rotationDirection;
+      rotationDirectionHasResetThisPeriod = true;
+      lastRotationDirectionToggleTime = data["t"];
+      digitalWrite(rotationDirectionPin, rotationDirection);
+    }
+    else if (static_cast<uint32_t>(data["t"]) - lastRotationDirectionToggleTime >= timeBetweenRotationDirectionToggles) {
+      rotationDirectionHasResetThisPeriod = false;
+    }
+
+    // Linear
+    if (!linearDirectionHasResetThisPeriod) {
+      linearDirection = !linearDirection;
+      linearDirectionHasResetThisPeriod = true;
+      lastLinearDirectionToggleTime = data["t"];
+      digitalWrite(linearDirectionPin, linearDirection);
+    }
+    else{
+      if (static_cast<uint32_t>(data["t"]) - lastLinearDirectionToggleTime >= timeBetweenLinearDirectionToggles) {
+        linearDirectionHasResetThisPeriod = true;
+      }
+    }
+
+
+
+    // Print to Serial Port
+    serializeJson(data, Serial);
+    Serial.print("\n");
+    cycle += 1;
+  }
+
+  /* Shutdown Cases */
+  if (static_cast<bool>(data["sdb"])) {
+    digitalWrite(rotationPulsePin, 0);
+    digitalWrite(linearPulsePin, 0);
+    return 1;
+  }
+  else if (static_cast<bool>(data["rb"])) {
+    digitalWrite(rotationPulsePin, 0);
+    digitalWrite(linearPulsePin, 0);
+    return 0;
+  }
+  else {
+    digitalWrite(rotationPulsePin, 0);
+    digitalWrite(linearPulsePin, 0);
+    return 2;
+  }
+}
